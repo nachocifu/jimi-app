53725ec1d87cf71f81ba53de8346bba3ec65920e
j1nma <j1nma@users.noreply.github.com>
2018-04-17 03:11:34 -0300

    [SETUP] [webapp] Added @PathVariable usage in helloWorld(). @RequestMapping can be used similarly. Example: http://localhost:8080/hello/Juanma

--- webapp/src/main/java/edu/itba/paw/jimi/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/edu/itba/paw/jimi/webapp/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package edu.itba.paw.jimi.webapp.controller;
 import edu.itba.paw.jimi.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,10 +13,10 @@ public class HelloWorldController {
 	@Autowired
 	private UserService us;
 	
-	@RequestMapping("/hello")
-	public ModelAndView helloWorld() {
+	@RequestMapping("/hello/{greeting}")
+	public ModelAndView helloWorld(@PathVariable("greeting") String greeting) {
 		final ModelAndView mav = new ModelAndView("index");
-		mav.addObject("greeting", "PAW");
+		mav.addObject("greeting", greeting);
 		return mav; 
 	}
 }
