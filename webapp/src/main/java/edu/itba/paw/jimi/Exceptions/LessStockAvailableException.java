7d3c5e203eeecfb831079d82d16c68b0478b81a1
Martín Capparelli <mcapparelli@itba.edu.ar>
2018-05-03 00:59:47 -0300

    [FEATURE][WEBAPP] Cool add dish view

--- webapp/src/main/java/edu/itba/paw/jimi/Exceptions/LessStockAvailableException.java
+++
@@ -1,8 +0,0 @@
-package edu.itba.paw.jimi.Exceptions;
-
-public class LessStockAvailableException extends IllegalArgumentException {
-
-    public LessStockAvailableException() {
-        super("Stock available is not enough.");
-    }
-}
