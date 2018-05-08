eec7991a5462def386697d4ea3ee170b60cca31c
nachocifu <ignacio.cifuentes948@gmail.com>
2018-05-08 17:59:57 -0300

    [FEATURE][SERVICES][@nacho] Implemented test and exception for handling adding more dishes than available to an order.

---
+++ interfaces/src/main/java/edu/itba/paw/jimi/interfaces/exceptions/StockHandlingException.java
@@ -0,0 +1,8 @@
+package edu.itba.paw.jimi.interfaces.exceptions;
+
+public class StockHandlingException extends RuntimeException {
+
+	public StockHandlingException(String message) {
+		super(message);
+	}
+}
