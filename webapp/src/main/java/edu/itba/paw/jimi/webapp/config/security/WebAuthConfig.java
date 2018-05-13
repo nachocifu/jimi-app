b62932b990b4d75154eff71ae5f18b2cd2bbac22
j1nma <j1nma@users.noreply.github.com>
2018-05-13 00:11:31 -0300

    [BUGFIX] [WEBAPP] Added webjars/** antMatchers for Safari.

--- webapp/src/main/java/edu/itba/paw/jimi/webapp/config/security/WebAuthConfig.java
+++ webapp/src/main/java/edu/itba/paw/jimi/webapp/config/security/WebAuthConfig.java
@@ -26,7 +26,7 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
-		
+		http.authorizeRequests().antMatchers("/webjars/**" ).permitAll();
 		http.userDetailsService(userDetailsService).sessionManagement()
 				.invalidSessionUrl("/login" ).and().authorizeRequests()
 				.antMatchers("/login" ).anonymous()
