f503ef7ab00419d7bd46d1135d0bb3a0dfacd8f8
Martín Capparelli <mcapparelli@itba.edu.ar>
2018-05-14 16:00:08 -0300

    [SERVICES][TEST] StatsService implemented methods tested

--- services/src/main/java/edu/itba/paw/jimi/services/StatsServiceImpl.java
+++ services/src/main/java/edu/itba/paw/jimi/services/StatsServiceImpl.java
@@ -1,10 +1,12 @@
 package edu.itba.paw.jimi.services;
 
+import edu.itba.paw.jimi.interfaces.daos.TableDao;
 import edu.itba.paw.jimi.interfaces.services.StatsService;
 import edu.itba.paw.jimi.models.Dish;
 import edu.itba.paw.jimi.models.DishStatus;
 import edu.itba.paw.jimi.models.Table;
 import edu.itba.paw.jimi.models.TableStatus;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +17,6 @@ public class StatsServiceImpl implements StatsService {
 
     private final int infBound = 50;
 
-    private TableStatus tableStatus;
-
-    private DishStatus dishStatus;
 
     @Autowired
     private DishServiceImpl dishService;
@@ -28,30 +27,31 @@ public class StatsServiceImpl implements StatsService {
     @Autowired
     private OrderServiceImpl orderService;
 
-    public int getBusyTables(){
-        return (int) ((getFreeTablesUnits() * 100.0) / tableService.findAll().size());
-    }
 
     public int getBusyTablesUnits() {
         int busy = 0;
         for (Table t : tableService.findAll()) {
-            if(t.getStatus() == tableStatus.Busy)
+            if(t.getStatus() == TableStatus.Busy)
                 busy += 1;
         }
         return busy;
     }
 
+    public int getBusyTables(){
+        return (int) ((getBusyTablesUnits() * 100.0) / tableService.findAll().size());
+    }
+
     public int getFreeTablesUnits() {
         int free = 0;
         for (Table t : tableService.findAll()) {
-            if(t.getStatus() == tableStatus.Free)
+            if(t.getStatus() == TableStatus.Free)
                 free += 1;
         }
         return free;
     }
 
     public int getFreeTables() {
-        return (int) ((getBusyTablesUnits() * 100.0) / tableService.findAll().size());
+        return (int) ((getFreeTablesUnits() * 100.0) / tableService.findAll().size());
     }
 
     public int getStockState() {
@@ -64,10 +64,12 @@ public class StatsServiceImpl implements StatsService {
     }
 
     public int getDinersToday() {
+        //TODO
         return 0;
     }
 
     public int getDishesSold() {
+        //TODO
         return 0;
     }
 
