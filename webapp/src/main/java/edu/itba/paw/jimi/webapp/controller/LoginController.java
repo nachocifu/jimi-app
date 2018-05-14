7b04c85dea1a794396c48bb5f5d3eb4e31c3c560
nachocifu <ignacio.cifuentes948@gmail.com>
2018-05-14 17:13:57 -0300

    [FEATURE] [WEBAPP] [@nachocifu @j1nma] On login success redirect handling based on user ROLE.

--- webapp/src/main/java/edu/itba/paw/jimi/webapp/controller/LoginController.java
+++ webapp/src/main/java/edu/itba/paw/jimi/webapp/controller/LoginController.java
@@ -1,15 +1,24 @@
 package edu.itba.paw.jimi.webapp.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
 	@RequestMapping("/login")
 	public ModelAndView login() {
 		return new ModelAndView("users/login");
+	}
+
+	@RequestMapping("/")
+	public ModelAndView onLoginSuccess(HttpServletRequest request) {
+		if (request.isUserInRole("ROLE_ADMIN"))
+			return new ModelAndView("/admin/index");
+		else
+			return new ModelAndView("/tables/list");
 	}
 	
 }
