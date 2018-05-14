89096ea83803a54052ecb9c0770290f077740812
j1nma <j1nma@users.noreply.github.com>
2018-05-14 18:55:15 -0300

    [TEST] [SERVICES] [@mcapparelli @j1nma] Fixed StatsServiceTest.

--- services/src/main/java/edu/itba/paw/jimi/services/StatsServiceImpl.java
+++ services/src/main/java/edu/itba/paw/jimi/services/StatsServiceImpl.java
@@ -1,76 +1,73 @@
 package edu.itba.paw.jimi.services;
 
-import edu.itba.paw.jimi.interfaces.daos.TableDao;
+import edu.itba.paw.jimi.interfaces.services.DishService;
+import edu.itba.paw.jimi.interfaces.services.OrderService;
 import edu.itba.paw.jimi.interfaces.services.StatsService;
+import edu.itba.paw.jimi.interfaces.services.TableService;
 import edu.itba.paw.jimi.models.Dish;
-import edu.itba.paw.jimi.models.DishStatus;
 import edu.itba.paw.jimi.models.Table;
 import edu.itba.paw.jimi.models.TableStatus;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-
 @Service
 public class StatsServiceImpl implements StatsService {
-
-    private final int infBound = 50;
-
-
-    @Autowired
-    private DishServiceImpl dishService;
-
-    @Autowired
-    private TableServiceImpl tableService;
-
-    @Autowired
-    private OrderServiceImpl orderService;
-
-
-    public int getBusyTablesUnits() {
-        int busy = 0;
-        for (Table t : tableService.findAll()) {
-            if(t.getStatus() == TableStatus.Busy)
-                busy += 1;
-        }
-        return busy;
-    }
-
-    public int getBusyTables(){
-        return (int) ((getBusyTablesUnits() * 100.0) / tableService.findAll().size());
-    }
-
-    public int getFreeTablesUnits() {
-        int free = 0;
-        for (Table t : tableService.findAll()) {
-            if(t.getStatus() == TableStatus.Free)
-                free += 1;
-        }
-        return free;
-    }
-
-    public int getFreeTables() {
-        return (int) ((getFreeTablesUnits() * 100.0) / tableService.findAll().size());
-    }
-
-    public int getStockState() {
-        int underBound = 0;
-        for(Dish d : dishService.findAll()){
-            if(d.getStock() <= infBound)
-                underBound += 1;
-        }
-        return (int) ((underBound * 100.0) / dishService.findAll().size()) ;
-    }
-
-    public int getDinersToday() {
-        //TODO
-        return 0;
-    }
-
-    public int getDishesSold() {
-        //TODO
-        return 0;
-    }
-
+	
+	private final int infBound = 50;
+	
+	@Autowired
+	private DishService dishService;
+	
+	@Autowired
+	private TableService tableService;
+	
+	@Autowired
+	private OrderService orderService;
+	
+	
+	public int getBusyTablesUnits() {
+		int busy = 0;
+		for (Table t : tableService.findAll()) {
+			if (t.getStatus() == TableStatus.Busy)
+				busy += 1;
+		}
+		return busy;
+	}
+	
+	public int getBusyTables() {
+		return (int) ((getBusyTablesUnits() * 100.0) / tableService.findAll().size());
+	}
+	
+	public int getFreeTablesUnits() {
+		int free = 0;
+		for (Table t : tableService.findAll()) {
+			if (t.getStatus() == TableStatus.Free)
+				free += 1;
+		}
+		return free;
+	}
+	
+	public int getFreeTables() {
+		return (int) ((getFreeTablesUnits() * 100.0) / tableService.findAll().size());
+	}
+	
+	public int getStockState() {
+		int underBound = 0;
+		for (Dish d : dishService.findAll()) {
+			if (d.getStock() <= infBound)
+				underBound += 1;
+		}
+		return (int) ((underBound * 100.0) / dishService.findAll().size());
+	}
+	
+	public int getDinersToday() {
+		//TODO
+		return 0;
+	}
+	
+	public int getDishesSold() {
+		//TODO
+		return 0;
+	}
+	
 }
--- services/src/test/java/edu/itba/paw/jimi/services/TableServiceTest.java
+++ services/src/test/java/edu/itba/paw/jimi/services/TableServiceTest.java
@@ -25,120 +25,120 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TableServiceTestConfig.class)
 public class TableServiceTest {
-
-    private static final String TABLE_NAME = "Table 1";
-    @InjectMocks
-    @Autowired
-    private TableService tableService;
-
-    @Autowired
-    @Mock
-    private TableDao tableDao;
-
-    @Autowired
-    @Mock
-    private OrderService orderService;
-
-    @Before
-    public void before(){
-        MockitoAnnotations.initMocks(this);
-    }
-    
-    @Test
-    public void createTest() {
-
-        // Mockito mocking
-        Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
-
-        Mockito.when(orderService.create(OrderStatus.INACTIVE, null, null, 0)).thenReturn(order);
-        Mockito.when(tableDao.create(TABLE_NAME, TableStatus.Free, order)).thenReturn(new Table(TABLE_NAME, 1, TableStatus.Free, order));
-        // Mockito mocking
-
-        Table table = tableService.create(TABLE_NAME);
-        assertEquals(TABLE_NAME, table.getName());
-        assertEquals(TableStatus.Free, table.getStatus());
-        assertEquals(order.getId(), table.getOrder().getId());
-        assertEquals(OrderStatus.INACTIVE, order.getStatus());
-        assertEquals(0, table.getOrder().getDiners());
-    }
-
-    @Test
-    public void setStatusFromFreeToBusyTest(){
-        Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
-        Table table = new Table(TABLE_NAME, 1, TableStatus.Free, order);
-
-        // Mockito mocking
-        Mockito.when(tableDao.findById(1)).thenReturn(table);
-        // Mockito mocking
-
-        tableService.changeStatus(table, TableStatus.Busy);
-
-        assertEquals(TableStatus.Busy, table.getStatus());
-    }
-
-    @Test(expected = TableStatusTransitionInvalid.class)
-    public void setStatusFromFreeToNOTBusyTest(){
-        Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
-        Table table = new Table(TABLE_NAME, 1, TableStatus.Free, order);
-
-        // Mockito mocking
-        Mockito.when(tableDao.findById(1)).thenReturn(table);
-        // Mockito mocking
-
-        tableService.changeStatus(table, TableStatus.Free);
-
-    }
-
-    @Test
-    public void setStatusFromBusyToCleaningTest(){
-        Order order = new Order(1, null, null, OrderStatus.OPEN,0 );
-        Table table = new Table(TABLE_NAME, 1, TableStatus.Busy, order);
-
-        // Mockito mocking
-        Mockito.when(tableDao.findById(1)).thenReturn(table);
-        // Mockito mocking
-
-        tableService.changeStatus(table, TableStatus.CleaningRequired);
-
-        assertEquals(TableStatus.CleaningRequired, table.getStatus());
-    }
-
-    @Test(expected = TableStatusTransitionInvalid.class)
-    public void setStatusFromBusyToNOTCleaningTest(){
-        Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
-        Table table = new Table(TABLE_NAME, 1, TableStatus.Busy, order);
-
-        // Mockito mocking
-        Mockito.when(tableDao.findById(1)).thenReturn(table);
-        // Mockito mocking
-
-        tableService.changeStatus(table, TableStatus.Free);
-
-    }
-
-    @Test
-    public void setStatusFromCleaningToFreeTest(){
-        Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
-        Table table = new Table(TABLE_NAME, 1, TableStatus.CleaningRequired, order);
-
-        // Mockito mocking
-        Mockito.when(tableDao.findById(1)).thenReturn(table);
-        // Mockito mocking
-
-        tableService.changeStatus(table, TableStatus.Free);
-
-        assertEquals(TableStatus.Free, table.getStatus());
-    }
-
-    @Test(expected = TableStatusTransitionInvalid.class)
-    public void setStatusFromCleaningToNOTFreeTest(){
-        Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
-        Table table = new Table(TABLE_NAME, 1, TableStatus.CleaningRequired, order);
-
-        // Mockito mocking
-        Mockito.when(tableDao.findById(1)).thenReturn(table);
-        // Mockito mocking
-
-        tableService.changeStatus(table, TableStatus.Busy);
-    }
+	
+	private static final String TABLE_NAME = "Table 1";
+	@InjectMocks
+	@Autowired
+	private TableService tableService;
+	
+	@Autowired
+	@Mock
+	private TableDao tableDao;
+	
+	@Autowired
+	@Mock
+	private OrderService orderService;
+	
+	@Before
+	public void before() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void createTest() {
+		
+		// Mockito mocking
+		Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
+		
+		Mockito.when(orderService.create(OrderStatus.INACTIVE, null, null, 0)).thenReturn(order);
+		Mockito.when(tableDao.create(TABLE_NAME, TableStatus.Free, order)).thenReturn(new Table(TABLE_NAME, 1, TableStatus.Free, order));
+		// Mockito mocking
+		
+		Table table = tableService.create(TABLE_NAME);
+		assertEquals(TABLE_NAME, table.getName());
+		assertEquals(TableStatus.Free, table.getStatus());
+		assertEquals(order.getId(), table.getOrder().getId());
+		assertEquals(OrderStatus.INACTIVE, order.getStatus());
+		assertEquals(0, table.getOrder().getDiners());
+	}
+	
+	@Test
+	public void setStatusFromFreeToBusyTest() {
+		Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
+		Table table = new Table(TABLE_NAME, 1, TableStatus.Free, order);
+		
+		// Mockito mocking
+		Mockito.when(tableDao.findById(1)).thenReturn(table);
+		// Mockito mocking
+		
+		tableService.changeStatus(table, TableStatus.Busy);
+		
+		assertEquals(TableStatus.Busy, table.getStatus());
+	}
+	
+	@Test(expected = TableStatusTransitionInvalid.class)
+	public void setStatusFromFreeToNOTBusyTest() {
+		Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
+		Table table = new Table(TABLE_NAME, 1, TableStatus.Free, order);
+		
+		// Mockito mocking
+		Mockito.when(tableDao.findById(1)).thenReturn(table);
+		// Mockito mocking
+		
+		tableService.changeStatus(table, TableStatus.Free);
+		
+	}
+	
+	@Test
+	public void setStatusFromBusyToCleaningTest() {
+		Order order = new Order(1, null, null, OrderStatus.OPEN, 0);
+		Table table = new Table(TABLE_NAME, 1, TableStatus.Busy, order);
+		
+		// Mockito mocking
+		Mockito.when(tableDao.findById(1)).thenReturn(table);
+		// Mockito mocking
+		
+		tableService.changeStatus(table, TableStatus.CleaningRequired);
+		
+		assertEquals(TableStatus.CleaningRequired, table.getStatus());
+	}
+	
+	@Test(expected = TableStatusTransitionInvalid.class)
+	public void setStatusFromBusyToNOTCleaningTest() {
+		Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
+		Table table = new Table(TABLE_NAME, 1, TableStatus.Busy, order);
+		
+		// Mockito mocking
+		Mockito.when(tableDao.findById(1)).thenReturn(table);
+		// Mockito mocking
+		
+		tableService.changeStatus(table, TableStatus.Free);
+		
+	}
+	
+	@Test
+	public void setStatusFromCleaningToFreeTest() {
+		Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
+		Table table = new Table(TABLE_NAME, 1, TableStatus.CleaningRequired, order);
+		
+		// Mockito mocking
+		Mockito.when(tableDao.findById(1)).thenReturn(table);
+		// Mockito mocking
+		
+		tableService.changeStatus(table, TableStatus.Free);
+		
+		assertEquals(TableStatus.Free, table.getStatus());
+	}
+	
+	@Test(expected = TableStatusTransitionInvalid.class)
+	public void setStatusFromCleaningToNOTFreeTest() {
+		Order order = new Order(1, null, null, OrderStatus.INACTIVE, 0);
+		Table table = new Table(TABLE_NAME, 1, TableStatus.CleaningRequired, order);
+		
+		// Mockito mocking
+		Mockito.when(tableDao.findById(1)).thenReturn(table);
+		// Mockito mocking
+		
+		tableService.changeStatus(table, TableStatus.Busy);
+	}
 }
