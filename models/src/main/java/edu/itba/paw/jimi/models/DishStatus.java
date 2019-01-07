0edafff74a4656cea4d0995b6c49e4a8c5bc4e56
j1nma <j1nma@users.noreply.github.com>
2019-01-07 19:40:15 -0300

    [CORRECTION] [MODELS] DishStatus toString() was not used.

--- models/src/main/java/edu/itba/paw/jimi/models/DishStatus.java
+++ models/src/main/java/edu/itba/paw/jimi/models/DishStatus.java
@@ -2,19 +2,5 @@ package edu.itba.paw.jimi.models;
 
 public enum DishStatus {
 	AVAILABLE,
-	UNAVAILABLE;
-	
-	@Override
-	public String toString() {
-		switch (this) {
-			case AVAILABLE:
-				return "Available";
-			
-			case UNAVAILABLE:
-				return "Unavailable";
-			
-			default:
-				return "No status found";
-		}
-	}
+	UNAVAILABLE
 }
