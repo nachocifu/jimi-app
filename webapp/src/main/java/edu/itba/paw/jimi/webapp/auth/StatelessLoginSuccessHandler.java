bbc9dafb829b19a26783a3f5b1560dbb782f1188
j1nma <j1nma@users.noreply.github.com>
2019-01-24 06:01:11 -0300

    [SETUP] [WEBAPP] Auth

---
+++ webapp/src/main/java/edu/itba/paw/jimi/webapp/auth/StatelessLoginSuccessHandler.java
@@ -0,0 +1,50 @@
+package edu.itba.paw.jimi.webapp.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class StatelessLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+	
+	@Autowired
+	private TokenAuthenticationService tokenAuthenticationService;
+	
+	private RequestCache requestCache = new HttpSessionRequestCache();
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws ServletException, IOException {
+		
+		tokenAuthenticationService.addAuthentication(response, authentication);
+		
+		SavedRequest savedRequest = requestCache.getRequest(request, response);
+		if (savedRequest == null) {
+			clearAuthenticationAttributes(request);
+			return;
+		}
+		
+		String targetUrlParam = getTargetUrlParameter();
+		if (isAlwaysUseDefaultTargetUrl() || (targetUrlParam != null && StringUtils.hasText(request.getParameter(targetUrlParam)))) {
+			requestCache.removeRequest(request, response);
+			clearAuthenticationAttributes(request);
+			return;
+		}
+		
+		clearAuthenticationAttributes(request);
+	}
+	
+	public void setRequestCache(RequestCache requestCache) {
+		this.requestCache = requestCache;
+	}
+}
