d6a8e999822ae2684709c97435d0be5ee8dabe67
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2019-01-28 18:38:03 -0300

    [FEATURE] [WEBAPP] Footer and Header components visual only.

--- reactUI/src/containers/DefaultLayout/DefaultFooter.js
+++ reactUI/src/containers/DefaultLayout/DefaultFooter.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Button from "reactstrap/es/Button";
 
 const propTypes = {
   children: PropTypes.node,
@@ -15,8 +16,12 @@ class DefaultFooter extends Component {
 
     return (
       <React.Fragment>
-        <span><a href="https://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
+        <span>JIMI &copy; 2019</span>
+        <span className="ml-auto">
+          <Button className="btn-github btn-brand ml-1 mr-1"><i
+            className="fa fa-github"></i><span>nachocifu</span></Button>
+          <Button className="btn-github btn-brand ml-1 mr-1"><i className="fa fa-github"></i><span>j1nma</span></Button>
+        </span>
       </React.Fragment>
     );
   }
