8cbf45cd532e04c5f1453a2819925215f54392e6
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2019-02-02 17:03:12 -0300

    [CORRECTIONS] [SERVICES, PERSISTENCE] Remove usage of custom QueryParams util from Table dao and service.

--- services/src/main/java/edu/itba/paw/jimi/services/TableServiceImpl.java
+++ services/src/main/java/edu/itba/paw/jimi/services/TableServiceImpl.java
@@ -27,19 +27,19 @@ import java.util.LinkedList;
 @Service
 public class TableServiceImpl implements TableService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(TableServiceImpl.class);
-	
+
 	@Autowired
 	private TableDao tableDao;
-	
+
 	@Autowired
 	@Qualifier(value = "userOrderService")
 	private OrderService orderService;
-	
+
 	@Override
 	public Table findById(final long id) {
 		return tableDao.findById(id);
 	}
-	
+
 	@Override
 	public Table create(String name) {
 		Order order = orderService.create(OrderStatus.INACTIVE, null, null, 0);
@@ -53,7 +53,7 @@ public class TableServiceImpl implements TableService {
 		}
 		return table;
 	}
-	
+
 	@Override
 	public Collection<Table> findAll() {
 		Collection<Table> tables = tableDao.findAll();
@@ -62,44 +62,54 @@ public class TableServiceImpl implements TableService {
 		else
 			return new HashSet<Table>();
 	}
-	
+
+	/**
+	 * @param qp
+	 * @return
+	 * @deprecated
+	 */
 	@Override
 	public Collection<Table> findAll(QueryParams qp) {
-		Collection<Table> tables = tableDao.findAll(qp);
+		return findAll(qp.getPageSize(), qp.getStartAt());
+	}
+
+	@Override
+	public Collection<Table> findAll(int maxResults, int offset) {
+		Collection<Table> tables = tableDao.findAll(maxResults, offset);
 		if (tables != null)
 			return tables;
 		else
 			return new HashSet<Table>();
 	}
-	
+
 	@Override
 	public Collection<Table> findTablesWithStatus(TableStatus tableStatus) {
 		return tableDao.findTablesWithStatus(tableStatus);
 	}
-	
+
 	@Override
 	public boolean tableNameExists(String tableName) {
 		return tableDao.tableNameExists(tableName);
 	}
-	
+
 	@Override
 	public int getTotalTables() {
 		return tableDao.getTotalTables();
 	}
-	
+
 	@Override
 	public void changeStatus(Table table, TableStatus status) {
-		
+
 		if (table.getStatus().equals(TableStatus.BUSY) && (!status.equals(TableStatus.PAYING) && !status.equals(TableStatus.FREE)))
 			throw new TableStatusTransitionInvalid(TableStatus.PAYING, status);
-		
+
 		if (table.getStatus().equals(TableStatus.PAYING) && !status.equals(TableStatus.FREE))
 			throw new TableStatusTransitionInvalid(TableStatus.FREE, status);
-		
+
 		if (table.getStatus().equals(TableStatus.FREE) && !status.equals(TableStatus.BUSY))
 			throw new TableStatusTransitionInvalid(TableStatus.BUSY, status);
-		
-		
+
+
 		switch (status) {
 			case BUSY: {
 				orderService.open(table.getOrder());
@@ -118,25 +128,25 @@ public class TableServiceImpl implements TableService {
 				break;
 			}
 		}
-		
+
 		table.setStatus(status);
 		table.setStatus(status);
 		tableDao.update(table);
 		LOGGER.info("Updated table {}", table);
 	}
-	
+
 	@Override
 	public int getNumberOfTablesWithState(TableStatus tableStatus) {
 		return tableDao.getNumberOfTablesWithState(tableStatus);
 	}
-	
+
 	@Override
 	public void setName(Table table, String name) {
 		table.setName(name);
 		tableDao.update(table);
 		LOGGER.info("Updated table name {}", table);
 	}
-	
+
 	@Override
 	public void delete(long id) {
 		Table table = tableDao.findById(id);
@@ -147,12 +157,12 @@ public class TableServiceImpl implements TableService {
 			LOGGER.info("Deleted table {}", table);
 		}
 	}
-	
+
 	@Override
 	public Collection<Table> getTablesWithOrdersFromLastMinutes(int minutes) {
 		if (minutes < 0)
 			return new LinkedList<>();
 		return tableDao.getTablesWithOrdersFromLastMinutes(minutes);
 	}
-	
+
 }
