96a310c889465fc1c10cf99bd74121013e19b9f5
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2019-02-03 20:43:52 -0300

    [SETUP] [WEBAPP] Setup and conf axios. Not Tested

---
+++ reactUI/src/http/RestClient.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import vars from '../../.env';
+
+export default class RestClient {
+  protocol;
+  domain;
+  port;
+  instance;
+
+  constructor() {
+    // Get Values From environment
+    this.protocol = vars.API_PROTOCOL;
+    this.domain = vars.API_DOMAIN;
+    this.port = vars.API_PORT;
+
+    // Create axios instance
+    this.instance = axios.create({
+      baseURL: `${this.protocol}://${this.domain}:${this.port}`,
+      timeout: 60000,
+    });
+  }
+}
---
+++ reactUI/src/http/clients/AuthRestClient.js
@@ -0,0 +1,22 @@
+import RestClient from "../RestClient";
+
+export default class AuthRestClient extends RestClient {
+
+  /**
+   *  Perform http request to signin
+   *  the user.
+   *
+   *  @param {string} username - The username.
+   *  @param {string} password - The password.
+   *  @param {string} application - The client.
+   *  @returns {Promise} - The http promise.
+   */
+  login(username: string, password: string) {
+    return this.instance.post(url, {
+        username: username,
+        password: password,
+      }
+    );
+  }
+
+}
