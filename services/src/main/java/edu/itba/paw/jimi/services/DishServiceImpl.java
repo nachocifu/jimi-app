0f506ebfb55c1358a2438867b9191e0d5246cfad
j1nma <j1nma@users.noreply.github.com>
2019-02-03 16:36:43 -0300

    [CLEANUP] [INTERFACES SERVICES] Rebase fix.

--- services/src/main/java/edu/itba/paw/jimi/services/DishServiceImpl.java
+++ services/src/main/java/edu/itba/paw/jimi/services/DishServiceImpl.java
@@ -47,13 +47,6 @@ public class DishServiceImpl implements DishService {
 	}
 
 	@Override
-	public void setName(Dish dish, String name) {
-		dish.setName(name);
-		dishDao.update(dish);
-		LOGGER.info("Updated dish name {}", dish);
-	}
-	
-	@Override
 	public int setStock(Dish dish, int stock) {
 		if (stock < 0) {
 			return 0;
