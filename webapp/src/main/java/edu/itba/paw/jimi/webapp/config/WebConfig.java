a7af820f3129a503810aa5196fd052529cc8ae41
j1nma <j1nma@users.noreply.github.com>
2019-02-04 20:34:38 -0300

    SmartLocaleResolver

--- webapp/src/main/java/edu/itba/paw/jimi/webapp/config/WebConfig.java
+++ webapp/src/main/java/edu/itba/paw/jimi/webapp/config/WebConfig.java
@@ -3,10 +3,7 @@ package edu.itba.paw.jimi.webapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
@@ -20,6 +17,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -35,14 +33,15 @@ import java.util.Properties;
 @ComponentScan({"edu.itba.paw.jimi.webapp.api", "edu.itba.paw.jimi.webapp.validators", "edu.itba.paw.jimi.webapp.utils", "edu.itba.paw.jimi.webapp.controller", "edu.itba.paw.jimi.services", "edu.itba.paw.jimi.persistence", "edu.itba.paw.jimi.models"})
 @Configuration
 @EnableTransactionManagement
+@PropertySource({"classpath:application.properties"})
 public class WebConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Value("classpath:schema.sql")
 	private Resource schemaSql;
-	
+
 	@Autowired
 	private Environment environment;
-	
+
 	@Bean
 	public DataSourceInitializer dataSourceInitializer(final DataSource ds) {
 		final DataSourceInitializer dsi = new DataSourceInitializer();
@@ -50,13 +49,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		dsi.setDatabasePopulator(databasePopulator());
 		return dsi;
 	}
-	
+
 	private DatabasePopulator databasePopulator() {
 		final ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
 		dbp.addScript(schemaSql);
 		return dbp;
 	}
-	
+
 	@Profile("development")
 	@Bean
 	public DataSource dataSource() {
@@ -67,7 +66,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		ds.setPassword("root");
 		return ds;
 	}
-	
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new SmartLocaleResolver();
+	}
+
 	@Bean
 	public MessageSource messageSource() {
 		final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
@@ -76,43 +80,43 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		messageSource.setCacheSeconds(5);
 		return messageSource;
 	}
-	
+
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-	
+
 	@Bean(name = "testName")
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(final DataSource dataSource) {
-		
+
 		final LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
 		factoryBean.setPackagesToScan("edu.itba.paw.jimi.models");
-		
+
 		factoryBean.setDataSource(dataSource);
-		
+
 		final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
-		
+
 		final Properties properties = new Properties();
 		properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL92Dialect");
-		
+
 		if (environment.acceptsProfiles("!production")) {
 			properties.setProperty("hibernate.show_sql", "true");
 			properties.setProperty("format_sql", "true");
 		}
-		
+
 		factoryBean.setJpaProperties(properties);
 		return factoryBean;
 	}
-	
+
 	@Bean
 	public PlatformTransactionManager transactionManager(final EntityManagerFactory emf) {
 		return new JpaTransactionManager(emf);
 	}
-	
+
 	@Bean
 	public Validator validator() {
 		return Validation.buildDefaultValidatorFactory().getValidator();
 	}
-	
+
 }
