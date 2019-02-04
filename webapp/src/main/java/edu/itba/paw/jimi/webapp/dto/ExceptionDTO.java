9cba93bb40b32984b498d2022faaaa98dec9ff58
j1nma <j1nma@users.noreply.github.com>
2019-02-04 20:27:42 -0300

    ExceptionDTO fix

--- webapp/src/main/java/edu/itba/paw/jimi/webapp/dto/ExceptionDTO.java
+++ webapp/src/main/java/edu/itba/paw/jimi/webapp/dto/ExceptionDTO.java
@@ -1,20 +1,20 @@
 package edu.itba.paw.jimi.webapp.dto;
 
 public class ExceptionDTO {
-	
+
 	private String message;
-	
+
 	public ExceptionDTO() {
 	}
-	
+
 	public ExceptionDTO(final String message) {
-		this.setMessage(message);
+		this.message = message;
 	}
-	
+
 	public String getMessage() {
 		return message;
 	}
-	
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
