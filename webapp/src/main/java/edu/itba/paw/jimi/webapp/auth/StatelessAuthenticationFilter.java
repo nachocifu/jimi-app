237ac5784154c3bddcfb47ffed33758d3c9d40d2
j1nma <j1nma@users.noreply.github.com>
2019-02-09 20:36:35 -0300

    catch JWTException to convertMessageToJWTErrorJson

--- webapp/src/main/java/edu/itba/paw/jimi/webapp/auth/StatelessAuthenticationFilter.java
+++ webapp/src/main/java/edu/itba/paw/jimi/webapp/auth/StatelessAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package edu.itba.paw.jimi.webapp.auth;
 
+import io.jsonwebtoken.JwtException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -11,19 +13,30 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 public class StatelessAuthenticationFilter extends GenericFilterBean {
-	
+
 	@Autowired
 	private TokenAuthenticationService tokenAuthenticationService;
-	
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		Authentication authentication = tokenAuthenticationService.getAuthentication((HttpServletRequest) request);
-		if (authentication != null) SecurityContextHolder.getContext().setAuthentication(authentication);
-		chain.doFilter(request, response);
-		SecurityContextHolder.getContext().setAuthentication(null);
+		try {
+			Authentication authentication = tokenAuthenticationService.getAuthentication((HttpServletRequest) request);
+			if (authentication != null) SecurityContextHolder.getContext().setAuthentication(authentication);
+			chain.doFilter(request, response);
+			SecurityContextHolder.getContext().setAuthentication(null);
+		} catch (JwtException e) {
+			response.getWriter().write(convertMessageToJWTErrorJson(e.getMessage()));
+			HttpServletResponse hsr = (HttpServletResponse) response;
+			hsr.setStatus(HttpStatus.UNAUTHORIZED.value());
+		}
+	}
+
+	private String convertMessageToJWTErrorJson(String message) {
+		return !message.isEmpty() ? "{ \"JWTerror\": \"" + message + "\" }" : null;
 	}
 }
