782a7c3311fb78dfae2c154fdcf4d5f5e919b66b
j1nma <j1nma@users.noreply.github.com>
2019-02-11 00:55:31 -0300

    KitchenDTO unnecessary, correct URI at kitchen tables

--- webapp/src/main/java/edu/itba/paw/jimi/webapp/api/KitchenApiController.java
+++ webapp/src/main/java/edu/itba/paw/jimi/webapp/api/KitchenApiController.java
@@ -2,16 +2,14 @@ package edu.itba.paw.jimi.webapp.api;
 
 import edu.itba.paw.jimi.interfaces.services.TableService;
 import edu.itba.paw.jimi.models.Table;
-import edu.itba.paw.jimi.webapp.dto.KitchenDTO;
+import edu.itba.paw.jimi.webapp.dto.TableListDTO;
 import edu.itba.paw.jimi.webapp.utils.PaginationHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.*;
+import java.net.URI;
 import java.util.Collection;
 import java.util.LinkedList;
 
@@ -32,14 +30,18 @@ public class KitchenApiController extends BaseApiController {
 	private UriInfo uriInfo;
 
 	@GET
+	@Path("/busyTables")
 	@Produces(value = {MediaType.APPLICATION_JSON})
 	public Response listKitchenTables(@QueryParam("page") @DefaultValue("1") Integer page,
 	                                  @QueryParam("pageSize") @DefaultValue("" + DEFAULT_PAGE_SIZE) Integer pageSize) {
 		page = paginationHelper.getPageAsOneIfZeroOrLess(page);
 		pageSize = paginationHelper.getPageSizeAsDefaultSizeIfOutOfRange(pageSize, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE);
 		Collection<Table> busyTables = tableService.getBusyTablesWithOrdersOrderedByOrderedAt(pageSize, (page - 1) * pageSize);
-		KitchenDTO kitchen = new KitchenDTO(new LinkedList<>(busyTables), buildBaseURI(uriInfo));
-		return Response.ok(kitchen)
+		URI tableURI = URI.create(String.valueOf(uriInfo.getBaseUri()) +
+				UriBuilder.fromResource(TableApiController.class).build() +
+				"/");
+		TableListDTO kitchenBusyTables = new TableListDTO(new LinkedList<>(busyTables), tableURI);
+		return Response.ok(kitchenBusyTables)
 				.links(paginationHelper.getPaginationLinks(uriInfo, page, busyTables.size()))
 				.build();
 	}
