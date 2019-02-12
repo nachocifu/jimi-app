bd6d468d89ff9402c6c0b08eaa985bea97d56c60
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2019-02-12 01:54:04 -0300

    Wired the kitchen screen

--- reactUI/src/http/clients/TableRestClient.js
+++ reactUI/src/http/clients/TableRestClient.js
@@ -92,4 +92,28 @@ export default class TableRestClient extends RestClient {
     );
   }
 
+  /**
+   *
+   * @param page
+   * @param pageSize
+   * @return Promise
+   */
+  getKitchen(page, pageSize) {
+    return this.instance.get(
+      'api/kitchen/busyTables'
+    );
+  }
+
+  /**
+   *
+   * @param table
+   * @param dish
+   * @return Promise
+   */
+  setDoneDish(table, dish) {
+    return this.instance.post(
+      'api/tables/'+table+'/undoneDishes/'+dish
+    );
+  }
+
 }
