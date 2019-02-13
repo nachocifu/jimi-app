fe70914781c90c2935411a0676ace49f77e600e4
j1nma <j1nma@users.noreply.github.com>
2019-02-13 04:07:34 -0300

    Dish Create and Edit Validations

--- reactUI/src/views/Dishes/Dishes.js
+++ reactUI/src/views/Dishes/Dishes.js
@@ -6,10 +6,6 @@ import {
   CardBody,
   CardHeader,
   Col,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
   Modal,
   ModalBody,
   ModalFooter,
@@ -21,10 +17,9 @@ import DishRestClient from '../../http/clients/DishRestClient'
 import Button from "reactstrap/es/Button";
 import Reactotron from "reactotron-react-js";
 import {connect} from "react-redux";
-import Form from "reactstrap/es/Form";
 import Spinner from "reactstrap/es/Spinner";
 import CardFooter from "reactstrap/es/CardFooter";
-
+import {AvField, AvForm} from 'availity-reactstrap-validation';
 
 function DishRow(props) {
   const dish = props.dish;
@@ -68,9 +63,16 @@ class Dishes extends Component {
     super(props);
     this.dishClient = new DishRestClient(props);
     this.downloadCsv = this.downloadCsv.bind(this);
-    this.state = {dishes: [], loading: true, form: {name: '', price: 0, stock: 0, minStock: 0}};
+    this.state = {
+      dishes: [],
+      loading: true,
+      modal: false,
+      form: {name: '', price: 0, stock: 0, minStock: 0, error: false}
+    };
     this.newDish = this.newDish.bind(this);
     this.toggle = this.toggle.bind(this);
+    this.handleValidSubmit = this.handleValidSubmit.bind(this);
+    this.handleInvalidSubmit = this.handleInvalidSubmit.bind(this);
   }
 
   toggle() {
@@ -88,7 +90,15 @@ class Dishes extends Component {
       this.state.form.price,
       this.state.form.minStock)
       .then(() => this.toggle())
-      .then(() => this.updateList());
+      .then(() => this.updateList())
+      .then(() => this.setState({loading: false}))
+      .catch((error) => {
+        Reactotron.error("Failed to create dish");
+
+        let form = {...this.state.form};
+        form.error = true;
+        this.setState({loading: false, form: form});
+      });
   }
 
   updateList() {
@@ -102,6 +112,27 @@ class Dishes extends Component {
 
   componentDidMount() {
     this.updateList();
+  }
+
+  handleValidSubmit(event, values) {
+    let form = {...this.state.form};
+    form.error = false;
+    form.name = values.name;
+    form.price = values.price;
+    form.stock = values.stock;
+    form.minStock = values.minStock;
+    this.setState({form: form});
+    this.newDish()
+  }
+
+  handleInvalidSubmit(event, errors, values) {
+    let form = {...this.state.form};
+    form.error = true;
+    form.name = values.name;
+    form.price = values.price;
+    form.stock = values.stock;
+    form.minStock = values.minStock;
+    this.setState({form: form});
   }
 
   downloadCsv() {
@@ -161,66 +192,45 @@ class Dishes extends Component {
           <ModalHeader toggle={this.toggle}>
             New Dish
           </ModalHeader>
-          <Form onSubmit={this.newDish}>
+          <AvForm onValidSubmit={this.handleValidSubmit} onInvalidSubmit={this.handleInvalidSubmit}>
             <ModalBody>
-              <InputGroup className="mb-3">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    Name
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input type="text" value={this.state.form.name}
-                       onChange={e => {
-                         let form = {...this.state.form};
-                         form.name = e.target.value;
-                         this.setState({form})
+              <AvField name="name" label="Name" type="text"
+                       validate={{
+                         required: {value: true, errorMessage: 'Please enter a name'},
+                         pattern: {
+                           value: '^[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)*$',
+                           errorMessage: 'Your name must be composed only with letter and numbers'
+                         },
+                         minLength: {value: 1, errorMessage: 'Your name must be between 1 and 25 characters'},
+                         maxLength: {value: 25, errorMessage: 'Your name must be between 1 and 25 characters'}
                        }}/>
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    Price
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input type="text" value={this.state.form.price}
-                       onChange={e => {
-                         let form = {...this.state.form};
-                         form.price = e.target.value;
-                         this.setState({form})
+              <AvField name="price" label="Price" type="number"
+                       validate={{
+                         required: {value: true, errorMessage: 'Please enter a price'},
+                         step: {value: 0.01},
+                         maxLength: {value: 10},
+                         min: {value: 1}
                        }}/>
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    Stock
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input type="text" value={this.state.form.stock}
-                       onChange={e => {
-                         let form = {...this.state.form};
-                         form.stock = e.target.value;
-                         this.setState({form})
+              <AvField name="stock" label="Stock" type="number"
+                       validate={{
+                         required: {value: true, errorMessage: 'Please enter a stock'},
+                         step: {value: 1},
+                         min: {value: 1},
+                         max: {value: 10000}
                        }}/>
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    Minimum Stock
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input type="text" value={this.state.form.minStock}
-                       onChange={e => {
-                         let form = {...this.state.form};
-                         form.minStock = e.target.value;
-                         this.setState({form})
+              <AvField name="minStock" label="Minimum Stock" type="number"
+                       validate={{
+                         required: {value: true, errorMessage: 'Please enter a minimum stock'},
+                         step: {value: 1},
+                         min: {value: 0},
+                         max: {value: 10000}
                        }}/>
-              </InputGroup>
             </ModalBody>
-            < ModalFooter>
+            <ModalFooter>
               <Button color="primary" className="px-4" block>Save</Button>
               <Button color="secondary" onClick={this.toggle}>Cancel</Button>
             </ModalFooter>
-          </Form>
+          </AvForm>
         </Modal>
       </div>
     )
