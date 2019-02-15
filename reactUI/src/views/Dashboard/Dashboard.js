a8b2ca403a4d98c03f21ffe4ccce978565c30cac
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2019-02-14 21:36:59 -0300

    Added percentages field on stats screen

--- reactUI/src/views/Dashboard/Dashboard.js
+++ reactUI/src/views/Dashboard/Dashboard.js
@@ -234,29 +234,14 @@ class Dashboard extends Component {
               <CardFooter>
                 <Row className="text-center">
                   <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">Visits</div>
-                    <strong>29.703 Users (40%)</strong>
-                    <Progress className="progress-xs mt-2" color="success" value="40"/>
+                    <div className="text-muted">Tables</div>
+                    <strong>{this.state.freeTablesPercentage}% Currently Free</strong>
+                    <Progress className="progress-xs mt-2" color="success" value={this.state.freeTablesPercentage}/>
                   </Col>
                   <Col sm={12} md className="mb-sm-2 mb-0 d-md-down-none">
-                    <div className="text-muted">Unique</div>
-                    <strong>24.093 Users (20%)</strong>
-                    <Progress className="progress-xs mt-2" color="info" value="20"/>
-                  </Col>
-                  <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">Pageviews</div>
-                    <strong>78.706 Views (60%)</strong>
-                    <Progress className="progress-xs mt-2" color="warning" value="60"/>
-                  </Col>
-                  <Col sm={12} md className="mb-sm-2 mb-0">
-                    <div className="text-muted">New Users</div>
-                    <strong>22.123 Users (80%)</strong>
-                    <Progress className="progress-xs mt-2" color="danger" value="80"/>
-                  </Col>
-                  <Col sm={12} md className="mb-sm-2 mb-0 d-md-down-none">
-                    <div className="text-muted">Bounce Rate</div>
-                    <strong>Average Rate (40.15%)</strong>
-                    <Progress className="progress-xs mt-2" color="primary" value="40"/>
+                    <div className="text-muted">Dishes</div>
+                    <strong>{this.state.stockStatePercentage}% Current Global Stock </strong>
+                    <Progress className="progress-xs mt-2" color="info" value={this.state.stockStatePercentage}/>
                   </Col>
                 </Row>
               </CardFooter>
